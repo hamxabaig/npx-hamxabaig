61a41c82c36858f229207c49562b27bdfd15701a
Hamza Baig <hamzabaig18@gmail.com>
2018-12-22 23:27:30 +0500

    adds card

---
+++ README.md
@@ -0,0 +1,10 @@
+## My Executive Developer Card :blush:
+
+### Usage
+Via npx:
+
+```
+npx hamxabaig
+```
+
+> :tada: [Make your own card](https://github.com/bnb/bitandbang) by [Tierney](https://github.com/bnb)
---
+++ build.js
@@ -0,0 +1,62 @@
+'use strict'
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('               Hamza Baig'),
+  handle: chalk.white('hamxabaig'),
+  work: chalk.white('Senior Full Stack Engineer @ListenField'),
+  opensource: chalk.white('Member Node.js Package Maintenance Team ') + chalk.green('⬢'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('hamxabaig_'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~hamxabaig'),
+  github: chalk.gray('https://github.com/') + chalk.green('hamxabaig'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('hamxabaig'),
+  web: chalk.cyan('https://hamxabaig.github.io'),
+  npx: chalk.red('npx') + ' ' + chalk.white('hamxabaig'),
+  labelWork: chalk.white.bold('       Work:'),
+  labelOpenSource: chalk.white.bold('Open Source:'),
+  labelTwitter: chalk.white.bold('    Twitter:'),
+  labelnpm: chalk.white.bold('        npm:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelWeb: chalk.white.bold('        Web:'),
+  labelCard: chalk.white.bold('       Card:')
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} / ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const npming = `${data.labelnpm}  ${data.npm}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.work
+               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
+               twittering + newline + // data.labelTwitter + data.twitter
+               npming + newline + // data.labelnpm + data.npm
+               githubing + newline + // data.labelGitHub + data.github
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               webing + newline + newline + // data.labelWeb + data.web
+               carding // data.labelCard + data.npx
+
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
